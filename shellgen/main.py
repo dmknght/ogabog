2ef71b857463e083993de7ad3befadd71c5ef556
Nong Hoang Tu <dmknght@parrotsec.org>
2020-12-27 01:17:30 +0700

    Better exception for import class

--- shellgen/main.py
+++ shellgen/main.py
@@ -19,12 +19,12 @@ else:
             # Import class with importlib
             # https://stackoverflow.com/a/17534365
             # initialize class to fix the problem can't use methods
-            module = getattr(module, class_name)()
+            try:
+                module = getattr(module, class_name)()
+            except AttributeError:
+                print("Invalid class name " + class_name + " for module " + module_name)
             # Parse args from cli, pass into module's args check
             module_args = module.get_opts().parse_args(un_args)
             module.show_shell(module_args)
         except ModuleNotFoundError:
             print("Invalid module name " + module_name)
-        except AttributeError:
-            # TODO confuse here between import class and other bugs
-            print("Invalid class name " + class_name + " for module " + module_name)
