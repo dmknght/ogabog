49c6d13b73f216d5cd2a5f07590981c1f83e3605
Nong Hoang Tu <dmknght@parrotsec.org>
2020-12-27 03:44:05 +0700

    Rename "main" to "genshell" and add description

--- shellgen/shellgen.py
+++ shellgen/shellgen.py
@@ -2,5 +2,5 @@ from ogabog.cores import argutils, controller
 import modules
 
 args = argutils.core_args()
-args.description = "Generate shell" # TODO improve description here
+args.description = "Generate bind / reverse shell"
 controller.program_handler(modules, args)
