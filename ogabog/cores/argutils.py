95f65461f272aa149af57f0472250c8a25cb14fe
Nong Hoang Tu <dmknght@parrotsec.org>
2020-12-27 01:35:01 +0700

    Add description for functions

--- ogabog/cores/argutils.py
+++ ogabog/cores/argutils.py
@@ -5,45 +5,58 @@ DEF_FLAG_MODULE = "-p"
 
 
 class ArgumentParser(argparse.ArgumentParser):
-    # Disable exit on error to get parameters of arguments
-    # https://stackoverflow.com/a/16942165
     def error(self, message):
+        """
+        Disable exit on error to get parameters of arguments
+        https://stackoverflow.com/a/16942165
+        :param message: error message
+        :return:
+        """
         print(message)
 
     def print_help(self, file=None):
+        """
+        Custom help module for argparse
+        If module is not defined, we print only help
+        If module is defined, we print hepl for module
+        :param file:
+        :return:
+        """
         try:
             # Find the args that contains "-p" flag
             pos = sys.argv.index(DEF_FLAG_MODULE)
             module_name = sys.argv[pos + 1]
+            # Ignore if argv starts with "-". Module name shouldn't have it
             if not module_name.startswith("-"):
-                print("Module " + module_name)
-                # This could be the actual name of module, we try import it
-                # Classes like bind / reverse in 1 module
                 # https://stackoverflow.com/a/22578562
                 # Old method with inspect to get class names and print help
+                # New method to get all names of classes in module
+                # https://stackoverflow.com/a/21563930
                 import importlib
                 module = importlib.import_module("modules." + module_name.replace("/", "."))
-                # New method to get all names of classes in module
-                # Try to print_help() with args
-                # https://stackoverflow.com/a/21563930
+                print("Module " + module_name)
                 for key, obj in module.__dict__.items():
                     if isinstance(obj, type):
                         print("\n" + key)
+                        # Show custom help of each module
                         obj().show_help(module_name, key)
 
             else:
                 # Wrong module name
                 self._print_message(self.format_help(), file)
-                self._print_message("\nInvalid module name\n")
+                self._print_message("\nInvalid module name " + module_name)
         except ValueError:
             self._print_message(self.format_help(), file)
         except ModuleNotFoundError:
             self._print_message(self.format_help(), file)
-            self._print_message("\nInvalid module name\n")
+            self._print_message("\nInvalid module name " + module_name)
 
 
 def core_args():
-    # https://github.com/zeropointdynamics/zelos/blob/0c5bd57b4bab56c23c27dc5301ba1a42ee054726/src/zelos/config_gen.py#L77
+    """
+    Add custom arguments for framework (core)
+    :return: ArgumentParser object
+    """
     parser = ArgumentParser()
     group_core = parser.add_argument_group("Core")
     group_core.add_argument(
