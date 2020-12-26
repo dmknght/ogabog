20327035155f3e4ff89b67239c935904655d60fe
Nong Hoang Tu <dmknght@parrotsec.org>
2020-12-27 01:10:05 +0700

    Add todo in descr and add show_shell

--- shellgen/main.py
+++ shellgen/main.py
@@ -17,10 +17,14 @@ else:
             module = importlib.import_module("modules." + module_name.replace("/", "."))
             # https://stackoverflow.com/a/41678146
             # Import class with importlib
+            # https://stackoverflow.com/a/17534365
+            # initialize class to fix the problem can't use methods
             module = getattr(module, class_name)()
             # Parse args from cli, pass into module's args check
             module_args = module.get_opts().parse_args(un_args)
+            module.show_shell(module_args)
         except ModuleNotFoundError:
             print("Invalid module name " + module_name)
         except AttributeError:
+            # TODO confuse here between import class and other bugs
             print("Invalid class name " + class_name + " for module " + module_name)
