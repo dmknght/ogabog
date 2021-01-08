4d998e5f9f6ea9295c01bf831cbb936fa168a9b3
Nong Hoang Tu <dmknght@parrotsec.org>
2021-01-08 22:33:45 +0700

    Fix typo

--- shellgen/modules/linux/expect.py
+++ shellgen/modules/linux/expect.py
@@ -10,7 +10,7 @@ class TTY(plugin.Module):
             choices=const.LINUX_SHELL,
             help="Select shell type on target machine"
         )
-        self.opts.description = "[TTYShell] Expect TTY shell escape.https://www.metahackers.pro/spawing-tty-shells/"
+        self.opts.description = "[TTYShell] Expect TTY shell escape. https://www.metahackers.pro/spawing-tty-shells/"
         self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
 
     def make_shell(self):
