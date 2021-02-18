e4d05826879a990aa02801c1be0e7b16d15f7bd9
Nong Hoang Tu <dmknght@parrotsec.org>
2021-02-18 16:23:41 +0700

    Add exception for KeyboardInterrupt

--- ogabog/cores/handler.py
+++ ogabog/cores/handler.py
@@ -171,5 +171,7 @@ def bind_tcp(ip, port, module_name, class_name, timeout):
         interpreter(cmd_prompt, sock_send, sock_recv)
     except Exception as error:
         print(f"[x] Error while connect to {ip}:{port}\n{error}")
+    except KeyboardInterrupt:
+        print("[*] Cancelled by user")
     finally:
         client.close()
