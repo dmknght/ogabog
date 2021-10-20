6b90257f1be7608def6e8fc608cefb44c63bf403
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-20 19:53:14 +0700

    Correct URL of description

--- shellgen/modules/linux/jjs.py
+++ shellgen/modules/linux/jjs.py
@@ -10,7 +10,7 @@ class PTY(plugin.Module):
             choices=const.LINUX_SHELL,
             help="Select shell type on target machine"
         )
-        self.opts.description = "[PTYShell] Java SE 8 PTY shell escape. https://gtfobins.github.io/gtfobins/jrunscript/"
+        self.opts.description = "[PTYShell] Java SE 8 PTY shell escape. https://gtfobins.github.io/gtfobins/jjs/"
         self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
         self.shell_type = "pty"
 
