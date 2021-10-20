ddd97e60f8cdca472f989db18004daaff2f49036
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-20 19:05:17 +0700

    Add jrunscript module

---
+++ shellgen/modules/linux/jrunscript.py
@@ -0,0 +1,18 @@
+from ogabog.cores import plugin, const
+
+
+class PTY(plugin.Module):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.opts.description = "[PTYShell] Expect PTY shell escape. https://www.metahackers.pro/spawing-tty-shells/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+        self.shell_type = "pty"
+
+    def make_shell(self):
+        self.shell = f"jrunscript -e \"exec('{self.args.shell} -c \\$@|sh _ echo sh <$(tty) >$(tty) 2>$(tty)')\""
