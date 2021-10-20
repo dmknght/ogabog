5a4bef9d8d9fe537c0667fd5f3c9fc86e34ddfac
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-20 19:41:52 +0700

    Add gem pty shell module

---
+++ shellgen/modules/linux/gem.py
@@ -0,0 +1,26 @@
+from ogabog.cores import plugin, const
+
+
+class PTY(plugin.Module):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.opts.description = "[PTYShell] Gem PTY shell escape. https://gtfobins.github.io/gtfobins/gem/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+        self.shell_type = "pty"
+
+    def make_shell(self):
+        # The Gem should be cross platform because it is a part of ruby interpreted language. But the command check is
+        # only available for Linux only and i didn't test on Windows environment
+        # In https://gtfobins.github.io/gtfobins/gem/, we have 4 different methods to execute PTY.
+        # This is the first method.
+        self.shell = f"gem open -e \"{self.args.shell} -c {self.args.shell}\" "
+        # In official doc, default gem "rdoc" is selected. However, test result on my system showed error
+        # 'rdoc' is a default gem and can't be opened. Show I added quick bash commands to get all gems are not
+        # default on the system to execute
+        self.shell += "$(gem list | grep -v default | head -1 | cut -d " " -f 1)"
