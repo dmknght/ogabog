7e65d1750a8683bed5f856e82c4a7026ee549a68
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-23 09:43:47 +0700

    add ghc module

---
+++ shellgen/modules/linux/ghc.py
@@ -0,0 +1,20 @@
+from ogabog.cores import plugin, const
+
+
+class Shell(plugin.BaseShell):
+    def __init__(self):
+        super().__init__()
+        self.add_args(
+            "--shell",
+            default="bash",
+            choices=const.LINUX_SHELL,
+            help="Select shell type on target machine"
+        )
+        self.opts.description = "https://gtfobins.github.io/gtfobins/ghc/"
+        self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
+        self.shell_type = 0
+        self.is_interactive = True
+
+    def make_shell(self):
+        self.shell = f"ghc -e 'System.Process.callCommand \"{self.args.shell}\"'"
+
