f0c37895c62a28ac9d1770774401d766561005f2
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-24 08:55:57 +0700

    Add ghci and code beauty for ghc

--- shellgen/modules/linux/ghci.py
+++ shellgen/modules/linux/ghci.py
@@ -10,10 +10,10 @@ class Shell(plugin.BaseShell):
             choices=const.LINUX_SHELL,
             help="Select shell type on target machine"
         )
-        self.opts.description = "https://gtfobins.github.io/gtfobins/ghc/"
+        self.opts.description = "https://gtfobins.github.io/gtfobins/ghci/"
         self.opts.description += "\nModule author: Nong Hoang Tu <dmknght@parrotsec.org>"
         self.shell_type = 0
         self.is_interactive = True
 
     def make_shell(self):
-        self.shell = f"ghc -e 'System.Process.callCommand \"{self.args.shell}\"'"
+        self.shell = f"ghci\nSystem.Process.callCommand \"{self.args.shell}\""
